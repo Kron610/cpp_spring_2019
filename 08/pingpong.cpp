3a12deeb93e2f16e9be690f600a79de30f7f08e8
Kron610 <ger.korekov@mail.ru>
2019-04-23 20:52:58 +0300

    homework_08 done

---
+++ 08/pingpong.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <mutex>
+#include <thread>
+#define COUNT 500000
+std::mutex mutex;
+std::mutex mutex2;
+void Ping()
+{
+	for (size_t i = 0; i < COUNT; i++)
+	{
+		mutex.lock();
+		std::cout << "ping\n";
+		mutex2.unlock();
+	}
+}
+
+void Pong()
+{
+	for (size_t i = 0; i < COUNT; i++)
+	{
+		mutex2.lock();
+		std::cout << "pong\n";
+		mutex.unlock();
+	}
+}
+
+int main()
+{
+	std::thread thread1(Ping);
+	mutex2.lock();
+	std::thread thread2(Pong);
+	thread1.join();
+	thread2.join();
+	return 0;
+}
